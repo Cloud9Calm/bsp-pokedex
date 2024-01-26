c63f59c723990c0a309bc4bf953a35deb610f7c2
erincronie <erincronie@gmail.com>
2024-01-26 13:59:32 -0800

    trying to add a dymanic background but will circle back later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import '../src/App.scss';
+import Background from './components/Background/Background';
 import Header from './components/Header/Header'
 import Pokemon from './components/Pokemon/Pokemon' 
 
 function App() {
   return (
     <div className="App">
+      <Background />
       <Header />
       <Pokemon />
     </div>
