47921a6f8ae88d82bfac7bcd6f5da147f6389937
erincronie <erincronie@gmail.com>
2024-01-26 20:46:46 -0800

    Merge branch 'main'

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import '../src/App.scss';
+import Header from './components/Header/Header';
+import PokemonList from './components/PokemonList/PokemonList';
+import PokemonDetails from './components/PokemonDetails/PokemonDetails'
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<PokemonList searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>} />
+        <Route path="/pokemon/:name" element={<PokemonDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
