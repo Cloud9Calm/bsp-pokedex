e029e05b72a290e4620bb48db40a6ed1c922029a
erincronie <erincronie@gmail.com>
2024-01-26 20:23:13 -0800

    adding back origin files

---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import './PokemonList.scss';
+import SearchBar from "../SearchBar/SearchBar";
+
+const PokemonList = ({ searchQuery, setSearchQuery }) => {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+  const API_URL = "https://pokeapi.co/api/v2/pokemon/?limit=151";
+
+  const fetchPokemonList = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      const data = response.data.results;
+
+      const pokemonWithImages = await Promise.all(
+        data.map(async (pokemon) => {
+          const response = await axios.get(pokemon.url);
+          return response.data;
+        })
+      );
+
+      setPokemonList(pokemonWithImages); 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemonList();
+  }, []);
+
+  useEffect(() => {
+    const filtered = pokemonList.filter(pokemon => pokemon.name.includes(searchQuery.toLowerCase()));
+    setFilteredPokemonList(filtered);
+  }, [searchQuery, pokemonList]);
+
+  return (
+    <section className="pokemon">
+      <h2 className="pokemon__title">151 Pokémon</h2>
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <ul className="pokemon__main-container">
+        {filteredPokemonList.map((pokemon, index) => ( // Use filteredPokemonList instead of pokemonList
+          <li className="pokemon__container" key={index}>
+            <Link to={`/pokemon/${pokemon.name}`} className="pokemon__link">
+              <img className="pokemon__img" src={pokemon.sprites.front_default} alt={pokemon.name}/>
+              <span className="pokemon__name">{pokemon.name}</span>
+            </Link>
+          </li>
+        ))}
+        </ul>
+    </section>
+  );
+};
+
+export default PokemonList;
+
