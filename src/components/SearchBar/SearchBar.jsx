4f367eb40c799cf18c33bec2f47af55ee704ad1b
erincronie <erincronie@gmail.com>
2024-02-08 10:57:26 -0800

    pokemon cards are more responsive between mobile to desktop

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './SearchBar.scss';
 
-const SearchBar = ({ searchQuery, setSearchQuery }) => {
+const SearchBar = ({ searchQuery, setSearchQuery, onSearch }) => {
   return (
     <div className='search-bar'>
       <label className='search-bar__label'>Search the Pokédex: </label>
       <input
-      className='search-bar__input'
+        className='search-bar__input'
         type='text'
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
