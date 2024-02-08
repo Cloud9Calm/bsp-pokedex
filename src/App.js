94e54e4187d319e401bf8a5d370e0bf9367d73ad
erincronie <erincronie@gmail.com>
2024-02-07 20:38:27 -0800

    updated pokemon card styling to have the pokemon slightly larger than the card, addeda hover effect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import '../src/App.scss';
 import Header from './components/Header/Header';
 import PokemonList from './components/PokemonList/PokemonList';
 import PokemonDetails from './components/PokemonDetails/PokemonDetails'
-import Background from './components/Background/Background'
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -12,7 +11,6 @@ function App() {
   return (
     <BrowserRouter>
     <Header />
-     <Background />
       <Routes>
         <Route path="/bsp-pokedex" element={<PokemonList searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>} />
         <Route path="/pokemon/:name" element={<PokemonDetails />} />
